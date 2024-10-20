cb9e049b73405f2ad8f687150d41823942c73a8f
Sayan Bhattacharjee <155574781+Syanx24@users.noreply.github.com>
2024-10-20 11:45:06 +0530

    Add files via upload

---
+++ Assignment6a.py
@@ -0,0 +1,8 @@
+val=input("Enter the numbers here: ").split()
+valint=[int(v) for v in val]
+#print(valint)
+sortedval=sorted(valint)
+print(sortedval)
+
+print(f"2nd lowest element is the list is {sortedval[1]}")
+print(f"2nd highest element is the list is {sortedval[-2]}")

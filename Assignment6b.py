092bcf449199dc8a06715b07e5d02cd37c380c9c
Sayan Bhattacharjee <155574781+Syanx24@users.noreply.github.com>
2024-10-22 20:56:20 +0530

    Add files via upload

--- Assignment6b.py
+++ Assignment6b.py
@@ -1,3 +1,4 @@
+#Given a list L write a program to make a new list and match the numbers inside list L to its respective index in the new list. Put 0 at remaining indexes. Also print the elements of the new list in the single line.
 val=input("Enter the numbers: ").split()
 valint=[int(v) for v in val]
 zerolist=[0]*(max(valint)+1)

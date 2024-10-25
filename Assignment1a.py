b1696c1b5d4ed342fb09a2123f881dc6b9ee41db
Sayan Bhattacharjee <155574781+Syanx24@users.noreply.github.com>
2024-10-25 12:58:20 +0530

    Add files via upload

---
+++ Assignment1a.py
@@ -0,0 +1,7 @@
+#Write a Python program to show MCKVIE with a print statement and Computer Science & Engineering with another print statement but in the same line.
+
+print("MCKVIE",end=",")
+print("Computer Science and Engineering")
+
+print("MCKVIE",end=" ") #not using "," in end
+print("Computer Science and Engineering")
